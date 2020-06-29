11e3e88cbf86f78e66984d1771130741d10fedae
abdelhay.aboulbaraket <Abdelhay.aboulbaraket@gmail.com>
2020-06-29 23:55:22 +0100

    final2

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const express = require ('express');
+const express = require("express");
 
-const app = express ();
+const app = express();
 
-app.use (express.static ('./dist/ebanking-client-front'));
+app.use(express.static("./dist/Ebanking-client-front"));
 
-app.get ('/*', (req, res) =>
-  res.sendFile ('index.html', {root: 'dist/ebanking-client-front/'})
+app.get("/*", (req, res) =>
+  res.sendFile("index.html", { root: "dist/Ebanking-client-front/" })
 );
 
-app.listen (process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080);
